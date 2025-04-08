6c750180e3930009a9cb589fe832c1eed1a49a80
Siddharth Lamba <122638975+lambasid@users.noreply.github.com>
2025-04-08 11:09:48 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,45 +1,109 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const cors = require("cors")
-const UserModel = require('./models/Users')
+// const express = require("express")
+// const mongoose = require('mongoose')
+// const cors = require("cors")
+// const UserModel = require('./models/Users')
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+// const app = express()
+// app.use(express.json())
+// app.use(cors())
 
+// mongoose.connect("mongodb://localhost:27017/user")
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
+
+//   app.post('/register', async (req, res) => {
+//     //Debugging Purposes
+//     console.log("Received data:", req.body);
+//     try {
+//         const user = await UserModel.create(req.body);
+//         res.json(user);
+//     } catch (err) {
+//         res.status(500).json({ error: err.message });
+//     }
+// });
+
+// app.post("/login", (req,res) => {
+//     const {email, password} = req.body;
+//     UserModel.findOne({email: email})
+//     .then(user => {
+//         if(user) {
+//             if(user.password === password) {
+//                 res.json("Success")
+//             } else {
+//                 res.join("the password is incorrect")
+//             }
+//         } else {
+//             res.json("No Record Exited")
+//         }
+//     })
+// })
+
+
+// app.listen(3001, () => {
+//     console.log("server is running")
+// })
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require('./models/Users'); // From user DB
+const MPModel = require('./models/MP'); // From DisabilityAdvocacy DB
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to "user" database for authentication
 mongoose.connect("mongodb://localhost:27017/user")
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("Connected to user DB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// ===== Auth Routes =====
 
-  app.post('/register', async (req, res) => {
-    //Debugging Purposes
-    console.log("Received data:", req.body);
-    try {
-        const user = await UserModel.create(req.body);
-        res.json(user);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+app.post('/register', async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-app.post("/login", (req,res) => {
-    const {email, password} = req.body;
-    UserModel.findOne({email: email})
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email })
     .then(user => {
-        if(user) {
-            if(user.password === password) {
-                res.json("Success")
-            } else {
-                res.join("the password is incorrect")
-            }
+      if (user) {
+        if (user.password === password) {
+          res.json("Success");
         } else {
-            res.json("No Record Exited")
+          res.json("The password is incorrect");
         }
-    })
-})
+      } else {
+        res.json("No Record Existed");
+      }
+    });
+});
 
+// ===== MP Search Route =====
+
+app.get('/mps', async (req, res) => {
+  const province = req.query.province;
+
+  try {
+    let filter = {};
+    if (province) {
+      filter = { "Province / Territory": { $regex: new RegExp(province, 'i') } };
+    }
+
+    const mps = await MPModel.find(filter);
+    res.json(mps);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.listen(3001, () => {
-    console.log("server is running")
-})
+  console.log("Server is running on port 3001");
+});
