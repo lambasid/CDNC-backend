4c5cf99474f96e7766a0e691f378c37fb21008b7
lambasid@sheridancollege.ca <lambasid@sheridancollege.ca>
2025-04-08 13:23:44 -0400

    Update database connection and add new routes for fetching users and MPs
    Signed-off-by: lambasid@sheridancollege.ca <lambasid@sheridancollege.ca>

--- index.js
+++ index.js
@@ -47,15 +47,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const UserModel = require('./models/Users'); // From user DB
-const MPModel = require('./models/MP'); // From DisabilityAdvocacy DB
+const UserModel = require('./models/UserModel'); // From user DB
+const MPModel = require('./models/MPModel'); // From DisabilityAdvocacy DB
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to "user" database for authentication
-mongoose.connect("mongodb://localhost:27017/user")
+mongoose.connect("mongodb+srv://itssidlamba:1234@cdnc-backend.qmiuwmr.mongodb.net/?retryWrites=true&w=majority&appName=cdnc-backend/user")
   .then(() => console.log("Connected to user DB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -104,6 +104,24 @@ app.get('/mps', async (req, res) => {
   }
 });
 
+app.get("/mps-all", async (req, res) => {
+  try {
+    const mps = await MPModel.find(); // Fetch all documents from the MPs collection
+    return res.status(200).json(mps);
+  } catch (error) {
+    console.error("Error fetching MPs:", error);
+    return res.status(500).json({ error: "An error occurred while fetching MPs" });
+  }
+});
+app.get("/user-all", async (req, res) => {
+  try {
+    const mps = await UserModel.find(); // Fetch all documents from the MPs collection
+    return res.status(200).json(mps);
+  } catch (error) {
+    console.error("Error fetching MPs:", error);
+    return res.status(500).json({ error: "An error occurred while fetching MPs" });
+  }
+});
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
